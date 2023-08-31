b6f8649b24b73ef7bfee1adac8b9fcfac6a29ae9
niu-grandpa <2864103063@qq.com>
2023-08-31 23:21:50 +0800

    public

---
+++ assets/clickoutside.780a9457.js
@@ -0,0 +1,1 @@
+import{m as c}from"./index.5a6af0e1.js";function p(e,i,n,s,t){const f=o=>{s?n.dataset[s]===t&&i(o):i()};c(document,"click",o=>f(o)),c(e,"click",o=>o.stopPropagation())}export{p as c};
---
+++ assets/one-node.0d15f947.js
@@ -0,0 +1,1 @@
+import{w as t}from"./index.5a6af0e1.js";function r(e){return e.childElementCount>1?(t(`The <${e.tagName.toLowerCase()}> tag must have a parent element`),!0):!1}export{r as m};
